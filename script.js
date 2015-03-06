b88f5ac2ce0f92e810495e778f72381ab9aa85ab
njlwisdom <njlwisdom@fullsail.edu>
2015-03-05 19:02:32 -0600

    Upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by njlwisdom on 3/5/15.
+ */
+
+
+
+// variables
+
+var MyFavoriteTypeofDog = "Siberian Husky"; //
+var AgeIEnteredArmy = "17"; //
+var Gay = "True"; //
+
+console.log("I have a favorite breed of dog and it is a," + MyFavoriteTypeofDog +"" );
+console.log("I made the bold decision to join the United States Army at the age of," + AgeIEnteredArmy +"" );
+console.log("It is " + Gay + " that I have lived with my same sex partner for over 5 years.");
